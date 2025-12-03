498cf1857b6768550bbd17d7530910a8f6a94df5
abidou572 <abdalux57290@gmail.com>
2025-12-03 14:10:25 +0100

    Update gameOfLife.h

---
+++ gameOfLife.h
@@ -0,0 +1,34 @@
+#include <unordered_set>
+#include <string>
+
+#include "grid.h"
+#include "display.h"
+#include "file.h"
+
+#include <SFML/Graphics.hpp>
+
+class GameOfLife {
+private:
+    int mode;
+    Grid grid;
+    Display* display;
+    sf::RenderWindow window;
+    File file;
+    int delay;
+    int maxGenerations;
+    int generation;
+    std::unordered_set<std::string> previousGrids;
+    
+bool checkStability();
+
+public:
+    GameOfLife();
+    ~GameOfLife();
+    void setFile(const std::string& filename);
+    void setDelay(int ms);
+    void setMaxGenerations(int max);
+    void setMode(int mode);
+    void start();
+
+    
+};
